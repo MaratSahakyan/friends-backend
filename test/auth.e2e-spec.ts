caa08404a44cc72777c6e4bd0c5da64a4c881c32
Marat Sahakyan <marat.sahakyan@team.gg>
2025-03-21 11:48:32 +0400

    [BE] - The auth test has been added

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -28,19 +28,6 @@ describe('AuthController (e2e)', () => {
     app = moduleFixture.createNestApplication();
     connection = moduleFixture.get<Pool>(DATABASE_POOL);
 
-    await connection.query(`
-    CREATE TABLE IF NOT EXISTS users (
-                                     id SERIAL PRIMARY KEY,
-                                     first_name VARCHAR(50) NOT NULL,
-                                     last_name VARCHAR(50) NOT NULL,
-                                     email VARCHAR(100) UNIQUE NOT NULL,
-                                     password_hash VARCHAR(255) NOT NULL,
-                                     age INT,
-                                     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                                     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
-    `);
-
     await clearTestDb();
 
     app.useGlobalPipes(
